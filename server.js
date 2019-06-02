9d5681d4b0278e9299b8d4639c315ed09b4194dc
Glitch (hello-express) <none>
2019-06-02 03:29:36 +0000

    🏓🏏 Checkpoint ./unsafeHashThing.js:104889/908 ./server.js:104889/1095

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ const app = express();
 const fs = require('fs');
 
 const wordList = fs.readFileSync('./eff_short_wordlist_1.txt', 'utf8');
+const adjDict = fs.readFileSync('./adj.txt', 'utf8').split('\n');
+const nounDict = fs.readFileSync('./noun.txt', 'utf8').split('\n');
 
 const dict = wordList.split('\n').map(e => {
     return e.split('\t')[1];
 });
 
 const unsafeHashWord = require('./unsafeHashWord.js')(dict);
+const unsafeHashThing = require('./unsafeHashThing.js')(adjDict, nounDict);
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -30,6 +33,11 @@ app.get('/', function(req, res) {
 app.get('/unsafehashword/:value', function(req, res) {
   console.log(req.params.value);
   res.send(unsafeHashWord(req.params.value));
+});
+
+app.get('/unsafehashthing/:value', function(req, res) {
+  console.log(req.params.value);
+  res.send(unsafeHashThing(req.params.value));
 });
 
 // listen for requests :)
