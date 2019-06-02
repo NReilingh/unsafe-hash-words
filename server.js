3e38fa96830f81692b371ce2d370f95eea7cdd6d
Glitch (hello-express) <none>
2019-06-02 04:59:47 +0000

    🏌🎲 Checkpoint ./server.js:104889/80

--- server.js
+++ server.js
@@ -9,11 +9,15 @@ const app = express();
 const fs = require('fs');
 
 const wordList = fs.readFileSync('./eff_short_wordlist_1.txt', 'utf8');
-const adjDict = fs.readFileSync('./adj.txt', 'utf8').split('\n');
-const nounDict = fs.readFileSync('./noun.txt', 'utf8').split('\n');
+const adjList = fs.readFileSync('./adj.txt', 'utf8').split('\n');
+const nounList = fs.readFileSync('./noun.txt', 'utf8').split('\n');
 
 const dict = wordList.split('\n').map(e => {
     return e.split('\t')[1];
+});
+
+const adjDict = dict.filter(e => {
+  adj
 });
 
 const unsafeHashWord = require('./unsafeHashWord.js')(dict);
