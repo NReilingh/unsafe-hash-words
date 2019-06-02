9623e62903ba94f45a8c407c4fd56c082b46e812
Glitch (concrete-smartphone) <none>
2019-06-02 05:52:50 +0000

    👯🏢 Checkpoint ./unsafeHashThing.js:104889/878

--- unsafeHashThing.js
+++ unsafeHashThing.js
@@ -4,15 +4,6 @@ const crypto = require('crypto');
 
 module.exports = function (adjDict, nounDict) {
   return function unsafeHashWord(stringData, length) {
-    if (length !== undefined) {
-      const adjlen = Math.ceil(length / 2);
-      const nounlen = Math.floor(length / 2);
-      
-      // Yup, these are function scope vars.
-      // This overrides our closure values with the same name for the duration of this function run.
-      var adjDict = adjDict.filter(e => { return e.length <= adjlen; });
-      var nounDict = nounDict.filter(e => { return e.length <= nounlen; });
-    }
     const md5 = crypto.createHash('MD5'); // intentionally not-cryptographically-secure
 
     md5.update(stringData, 'utf8');
@@ -21,6 +12,15 @@ module.exports = function (adjDict, nounDict) {
     
     const front = '0x' + hash.substring(1,16);
     const back = '0x' + hash.substring(16);
+
+    if (length !== undefined) {
+      const adjlen = Math.ceil(length / 2);
+      const nounlen = Math.floor(length / 2);
+      
+      const iAdjDict = adjDict.filter(e => { return e.length <= adjlen; });
+      const iNounDict = nounDict.filter(e => { return e.length <= nounlen; });
+      return iAdjDict[BigInt(front) % BigInt(iAdjDict.length)] + iNounDict[BigInt(back) % BigInt(iNounDict.length)];
+    }
     
     return adjDict[BigInt(front) % BigInt(adjDict.length)] + nounDict[BigInt(back) % BigInt(nounDict.length)];
   };
