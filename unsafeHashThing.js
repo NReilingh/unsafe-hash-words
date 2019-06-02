94c3ca32a61ba70aad8a2d2bf2cbc169b4117215
Glitch (concrete-smartphone) <none>
2019-06-02 05:26:45 +0000

    🐜👋 Checkpoint ./unsafeHashNoun.js:104889/536 ./unsafeHashThing.js:104889/774

--- unsafeHashThing.js
+++ unsafeHashThing.js
@@ -2,17 +2,12 @@
 
 const crypto = require('crypto');
 
-module.exports = function (adjDict, nounDict) {
+module.exports = function (dict) {
   return function unsafeHashWord(stringData) {
     const md5 = crypto.createHash('MD5'); // intentionally not-cryptographically-secure
 
     md5.update(stringData, 'utf8');
     
-    const hash = md5.digest('hex');
-    
-    const front = '0x' + hash.substring(1,16);
-    const back = '0x' + hash.substring(16);
-    
-    return adjDict[BigInt(front) % BigInt(adjDict.length)] + nounDict[BigInt(back) % BigInt(nounDict.length)];
+    return dict[BigInt('0x' + md5.digest('hex')) % BigInt(dict.length)];
   };
 }
