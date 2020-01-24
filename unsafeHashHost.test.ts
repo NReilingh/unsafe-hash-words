93dac49de6d4ca26fe14485d9e60a8151d61461a
Nick Reilingh <nreiling@bard.edu>
2020-01-24 01:32:54 -0500

    added some more tests

--- unsafeHashHost.test.ts
+++ unsafeHashHost.test.ts
@@ -1,5 +1,9 @@
 import { unsafeHashHost } from './unsafeHashHost';
 
-test('some random result', () => {
-  expect(unsafeHashHost('G97VVY1', 8)).toBe('gamerash')
+test.each([
+  ['G97VVY1', 'gamerash'],
+  ['VMware-56 4d 18 de 52 41 ac 4b-01 85 94 e1 0e 71 83 48', 'plusmost'],
+  ['C07YG0X0JYW0', 'neonecho']
+])('test %s and 8-char limit results in %s', (input, expected) => {
+  expect(unsafeHashHost(input, 8)).toBe(expected)
 });
